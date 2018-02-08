e0713546a2361932fa8c040eccb401522122de1b
TheRiven <Novajames276@gmail.com>
2018-02-08 12:52:03 +0000

    End of XOR tutorial state

--- JS/Neural_Network_Project.js
+++ JS/Neural_Network_Project.js
@@ -1,35 +1,49 @@
 let network;
 let training_data = [
     {
-        inputs: [1, 0],
-        targets: [1]
-    },
-    {
-        inputs: [0, 1],
-        targets: [1]
-    },
-    {
         inputs: [0, 0],
         targets: [0]
     },
     {
         inputs: [1, 1],
         targets: [0]
+    },
+    {
+        inputs: [1, 0],
+        targets: [1]
+    },
+    {
+        inputs: [0, 1],
+        targets: [1]
     }
 ];
+let lr_slider;
 function setup() {
+    createCanvas(400, 400);
     network = new NeuralNetwork(2, 2, 1);
-    for (let i = 0; i < 50000; i++) {
+    lr_slider = createSlider(0.01, 0.1, 0.05, 0.01);
+}
+function draw() {
+    background(0);
+    network.learning_rate = lr_slider.value();
+    for (let i = 0; i < 100; i++) {
         let data = random(training_data);
         network.train(data.inputs, data.targets);
     }
-    console.table(network.feedforward([0, 1]));
-    console.table(network.feedforward([1, 0]));
-    console.table(network.feedforward([0, 0]));
-    console.table(network.feedforward([1, 1]));
-}
-function draw() {
-}
-function doubleIt(x) {
-    return x * 2;
+    let resolution = 20;
+    let cols = Math.floor(width / resolution);
+    let rows = Math.floor(height / resolution);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            let input_1 = i / (cols - 1);
+            let input_2 = j / (rows - 1);
+            let output = network.feedforward([input_1, input_2]);
+            let color = output[0] * 255;
+            fill(color);
+            noStroke();
+            rect(x, y, resolution, resolution);
+        }
+    }
 }
--- TS/Neural_Network_Project.ts
+++ TS/Neural_Network_Project.ts
@@ -1,48 +1,75 @@
+// Quick fix for p5 javascript functions and values
 declare let random: Function;
+declare let createCanvas: Function;
+declare let background: Function;
+declare let rect: Function;
+declare let fill: Function;
+declare let noStroke: Function;
+declare let createSlider;
+declare let width: number;
+declare let height: number;
+
 
 let network : NeuralNetwork;
 
 let training_data = [
     {
-        inputs: [1,0],
-        targets: [1]
-    },
-    {
-        inputs: [0,1],
-        targets: [1]
-    },
-    {
-        inputs: [0,0],
+        inputs: [0, 0],
         targets: [0]
     },
     {
-        inputs: [1,1],
+        inputs: [1, 1],
         targets: [0]
+    },
+    {
+        inputs: [1, 0],
+        targets: [1]
+    },
+    {
+        inputs: [0, 1],
+        targets: [1]
     }
-]
+];
+
+let lr_slider;
 
 function setup() 
 {
+    createCanvas(400, 400);
     network = new NeuralNetwork(2, 2, 1);
 
-    for (let i = 0; i < 50000; i++) {
-        let data = random(training_data);
-
-        network.train(data.inputs, data.targets);        
-    }
-    
-    console.table(network.feedforward([0, 1]))
-    console.table(network.feedforward([1, 0]))
-    console.table(network.feedforward([0, 0]))
-    console.table(network.feedforward([1, 1]))
+    lr_slider = createSlider(0.01, 0.1, 0.05, 0.01);
 }
 
 function draw() 
 {
-    
-}
+    background(0);
 
-function doubleIt(x : number) 
-{
-    return x * 2;
+    network.learning_rate = lr_slider.value();
+
+    for (let i = 0; i < 100; i++) {
+        let data = random(training_data);
+        network.train(data.inputs, data.targets);
+    }
+
+    let resolution = 20;
+    let cols = Math.floor(width / resolution);
+    let rows = Math.floor(height / resolution);
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+
+            let input_1 = i / (cols - 1);
+            let input_2 = j / (rows - 1);
+            let output = network.feedforward([input_1, input_2]);
+            let color = output[0] * 255;
+
+            fill(color);
+            noStroke();
+            rect(x, y, resolution, resolution);
+        }    
+    }
+
 }
