961fbe9a564610a54227729d91e5dddde7dba3cd
TheRiven <Novajames276@gmail.com>
2018-02-08 18:18:02 +0000

    Adds more checks to Matrix.js

--- JS/matrix.test.js
+++ JS/matrix.test.js
@@ -4,3 +4,19 @@ test('Create a Matrix with 2 cols and 3 rows', () => {
     expect(matrix.rows).toBe(3);
     expect(matrix.cols).toBe(2);
 });
+test('Adding scalar to a matrix', () => {
+    let m = new matrixJS(3, 3);
+    m.data[0] = [1, 2, 3];
+    m.data[1] = [4, 5, 6];
+    m.data[2] = [7, 8, 9];
+    m.add(1);
+    expect(m).toEqual({
+        data: [
+            [2, 3, 4],
+            [5, 6, 7],
+            [8, 9, 10]
+        ],
+        cols: 3,
+        rows: 3
+    });
+});
