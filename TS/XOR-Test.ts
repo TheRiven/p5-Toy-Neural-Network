5a00940333709937f0e9350a16d8c5e6d53c9aae
TheRiven <Novajames276@gmail.com>
2018-02-11 12:30:50 +0000

    Move p5 helpers out of XOR test

--- TS/XOR-Test.ts
+++ TS/XOR-Test.ts
@@ -1,15 +1,3 @@
-// Quick fix for p5 javascript functions and values
-declare let random: Function;
-declare let createCanvas: Function;
-declare let background: Function;
-declare let rect: Function;
-declare let fill: Function;
-declare let noStroke: Function;
-declare let createSlider;
-declare let width: number;
-declare let height: number;
-
-
 let network : NeuralNetwork;
 
 let training_data = [
---
+++ TS/p5funcs.ts
@@ -0,0 +1,10 @@
+// Quick fix for p5 javascript functions and values
+declare let random: Function;
+declare let createCanvas: Function;
+declare let background: Function;
+declare let rect: Function;
+declare let fill: Function;
+declare let noStroke: Function;
+declare let createSlider;
+declare let width: number;
+declare let height: number;
